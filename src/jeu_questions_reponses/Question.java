dab2f4a4e119c30f9446f9e15a78fb4051a20db0
Adigbonon <mahoutondji.adigbonon@iut-rodez.fr>
2021-11-17 14:03:59 +0100

    classes relatives aux themes/questions

--- src/jeu_questions_reponses/Question.java
+++ src/jeu_questions_reponses/Question.java
@@ -1,5 +1,108 @@
 package jeu_questions_reponses;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Question {
 
+	private int numero;	
+	private String type;
+	private String theme;
+	private String niveau;
+	private String enonce;
+	private List <String> reponses = new ArrayList<>();
+	
+	public static String table_difficultes[] = {"Facile", "Moyen", "Difficile"};
+
+	public static Scanner entree = new Scanner (System.in);
+	
+	public Question() {
+		
+	}
+	
+	public Question(int numero, String type, String theme, String niveau, String enonce, List <String> reponses) {
+		this.numero = numero;
+		this.type = type;
+		this.theme = theme;
+		this.niveau = niveau;
+		this.enonce = enonce;
+		this.reponses = reponses;
+	}
+	
+	public void affichQuestion(Questions questions, int numero) {
+		Question question_a_afficher = questions.getListe().get(numero - 1);		
+		
+		System.out.println("Thème : " + question_a_afficher.theme + "\nNiveau : " +  
+						   question_a_afficher.niveau + "\nQuestion " + (numero - 1) + " : "
+						   + question_a_afficher.enonce);
+		for(int i = 0;  i < question_a_afficher.reponses.size(); i++) {
+				System.out.println((i+1) + " - " + question_a_afficher.reponses.get(i));
+		}
+	}
+	
+	public static Question saisieQuestion(String theme_quest) {
+		Question question_a_retourner = new Question();
+		
+		int numero_type;
+		int numero_niveau;
+		String bonne_reponse;
+		String autre_reponse;
+		
+		question_a_retourner.theme = theme_quest;
+		System.out.println("Quel est le type de la question? : \n1- VF\n2- RC\n3- QCM");
+		numero_type = entree.nextInt();
+		
+		if(numero_type == 1 || numero_type == 2 || numero_type == 3) { 
+			System.out.println("Entrez l'énoncé de la question : ");
+			question_a_retourner.enonce = entree.nextLine();
+			
+			System.out.println("Quel est le niveau de la difficulté?");
+			for(int i = 0; i < table_difficultes.length; i++) {
+				System.out.println((i+1) + " - " + table_difficultes[i]);
+			}
+			numero_niveau = entree.nextInt();
+			
+			if(numero_niveau > table_difficultes.length - 1 || numero_niveau < 0) {
+				System.out.println("Choisissez le niveau dans la liste ci-dessus");
+			} else {
+				question_a_retourner.niveau = table_difficultes[numero_niveau + 1];
+				
+				System.out.println("Entrez l'énoncé de la question :");
+				question_a_retourner.enonce = entree.nextLine();
+				
+				System.out.println("Entrez la bonne réponse :");
+				bonne_reponse = entree.nextLine();
+				
+				question_a_retourner.reponses.add(bonne_reponse);
+				
+				if(numero_type == 1) {
+					question_a_retourner.type = "VF";
+				} else if(numero_type == 2){
+					question_a_retourner.type = "RC";
+				} else {
+					question_a_retourner.type = "QCM";
+				}
+				
+				if(numero_type == 1) {					
+					System.out.println("Entrez la mauvaise réponse :");
+					autre_reponse = entree.nextLine();
+					question_a_retourner.reponses.add(autre_reponse);
+				} else if(numero_type == 3) {					
+					System.out.println("Entrez une mauvaise réponse :");
+					autre_reponse = entree.nextLine();
+					question_a_retourner.reponses.add(autre_reponse);
+					
+					System.out.println("Entrez une autre mauvaise réponse :");
+					autre_reponse = entree.nextLine();
+					question_a_retourner.reponses.add(autre_reponse);
+				}		
+			}
+		} else  {
+			System.out.println("Choisissez un type dans la liste ci-dessus");
+		}
+		
+		return question_a_retourner;
+	}
+	
 }
--- src/jeu_questions_reponses/Theme.java
+++ src/jeu_questions_reponses/Theme.java
@@ -1,14 +1,48 @@
 package jeu_questions_reponses;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.List;
 
 public class Theme {
 	
 	private static String designation;
 	private static int nb_questions;
-	private static List <String> liste_questions = new ArrayList<>();
-	
-	
+	private static int indicateur;
+	private static Questions liste_questions = new Questions();	
 
+	public Theme() {
+		Random r = new Random();
+		this.nb_questions = r.nextInt(10-5) + 5;
+	}
+	
+	public Theme(String designation, int nb_questions, int indicateur) {
+		this.designation = designation;
+		this.nb_questions = nb_questions;
+		this.indicateur = indicateur;
+	}
+	
+	public int getIndicateur(){
+		return this.indicateur;
+	}
+	
+	public String getDesignation(){
+		return this.designation;
+	}
+	
+	public void setDesignation(String designation){
+		this.designation = designation;
+	}
+	
+	public void setNbQuestions(int nb_questions){
+		this.nb_questions = nb_questions;
+	}
+	
+	public void setIndicateur(int indicateur){
+		this.indicateur = indicateur;
+	}
+	
+	public void setQuestions(Questions liste){
+		this.liste_questions = liste;
+	}
 }
